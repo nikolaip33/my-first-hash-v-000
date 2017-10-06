d0a7050e54cfbecf42f1bda8aecb51009d645c7b
Nikolai Pawlowski <nikolaip@gmail.com>
2017-10-06 06:56:32 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,14 +1,21 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
-  
+  dictionary = {
+    "apple" => "a delicious fruit",
+    "this readme" => "wonderful and informative piece of reading",
+    "Moby Dick" => "a novel by my good friend, Herman Melville"
+  }
+
+
+
 end
 
 
 def shipping_manifest
   #set a variable called `shipping_manifest`, equal to a hash
-  #fill that hash with key/value pairs that describe the following information: 
-  #We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
+  #fill that hash with key/value pairs that describe the following information:
+  #We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
 
 
 end
@@ -21,11 +28,8 @@ end
 
 def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-  
+
   #your code here
   #remember to return the shipping_manifest hash
 
 end
-
-
-
