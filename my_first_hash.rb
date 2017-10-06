40fadd0a37aebf95f3a1ebddaa9d77c9a9b0f591
Nikolai Pawlowski <nikolaip@gmail.com>
2017-10-06 06:59:53 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -30,6 +30,7 @@ def retrieval
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 
   #your code here!
+  shipping_manifest["oil paintings"]
 end
 
 def adding
